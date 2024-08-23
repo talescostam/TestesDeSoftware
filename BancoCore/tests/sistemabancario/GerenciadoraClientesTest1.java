86970ba48c7180718e3232ea89dcf4e04c9303ca
tales.costa <talescostam@gmail.com>
2024-08-23 19:55:14 -0300

    Add testRemoveCliente

--- BancoCore/tests/sistemabancario/GerenciadoraClientesTest1.java
+++ BancoCore/tests/sistemabancario/GerenciadoraClientesTest1.java
@@ -35,4 +35,26 @@ public class GerenciadoraClientesTest1 {
 
     }
 
+    @Test
+    public void testRemoveCliente() {
+
+        //Criando alguns clientes
+        Cliente cliente1 = new Cliente(1, "Tales", 32, "tales@gmail.com", 1, true);
+        Cliente cliente2 = new Cliente(2, "Clayton", 48, "clayton@gmail.com", 2, true);
+
+        //Inserindo os clientes criados na lista de clientes do banco
+        List<Cliente> clientesDoBanco = new ArrayList<>();
+        clientesDoBanco.add(cliente1);
+        clientesDoBanco.add(cliente2);
+
+        GerenciadoraClientes gerenciadoraClientes = new GerenciadoraClientes(clientesDoBanco);
+
+        //Execução do teste
+        Boolean deletou = gerenciadoraClientes.removeCliente(1);
+
+        //Avaliação do teste
+        assertThat(deletou.booleanValue(), CoreMatchers.is(true));
+
+    }
+
 }
